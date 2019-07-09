55cf79d9b3ff547921473e93a591e37e8608ff08
Bob <bob.e.baxter@gmail.com>
2019-07-08 21:04:13 -0500

    delete works

--- src/helpers/data/scatData.js
+++ src/helpers/data/scatData.js
@@ -18,4 +18,6 @@ const getScat = uid => new Promise((resolve, reject) => {
     .catch(err => reject(err));
 });
 
-export default { getScat };
+const deleteScat = scatId => axios.delete(`${baseUrl}/scats/${scatId}.json`);
+
+export default { getScat, deleteScat };
