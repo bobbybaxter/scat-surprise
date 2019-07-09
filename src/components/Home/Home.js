624ea32c26d89a1bba693b16f313c177d315381c
Bob <bob.e.baxter@gmail.com>
2019-07-08 20:02:53 -0500

    reading cards works

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import scatData from '../../helpers/data/scatData';
+import ScatCard from '../ScatCard/ScatCard';
 
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    scats: [],
+  }
+
   editEvent = (e) => {
     e.preventDefault();
     const orderId = '12345'; // usually e.target.something
     this.props.history.push(`/edit/${orderId}`);
   };
 
+  componentDidMount() {
+    const { uid } = firebase.auth().currentUser;
+    scatData.getScat(uid)
+      .then(response => this.setState({ scats: response }))
+      .catch(err => console.error(err));
+  }
+
   render() {
     const singleLink = '/scat/12345';
+    const makeScatCards = this.state.scats.map(scat => (
+      <ScatCard
+        key={scat.id}
+        scat={scat}
+      />
+    ));
     return (
       <div className="Home col">
         <h1>Home</h1>
-        {/* can use this */}
-        <button className="btn btn-dark" onClick={this.editEvent}>Edit a thing</button>
-        {/* or this */}
-        <Link to={singleLink}>View Single</Link>
+        <div className="buttons d-flex flex-row container justify-content-around">
+          {/* can use this */}
+          <button className="btn btn-dark" onClick={this.editEvent}>Edit a thing</button>
+          {/* or this */}
+          <Link to={singleLink}>View Single</Link>
+        </div>
+        <div className="d-flex flex-row container justify-content-around mt-5">
+          {makeScatCards}
+        </div>
       </div>
     );
   }
